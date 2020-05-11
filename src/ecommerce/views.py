7a60d65e0c9d648e17cbadca5a960c5126af3cb2
HervalMata <hervalmataamparo@gmail.com>
2020-05-11 18:22:37 -0300

    Template Context

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hello World!",
+        "content": "Bemvindo ao site"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Sobre",
+        "content": "Tudo sobre o site"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contato",
+        "content": "Deixe-nos entrar em contato"
+    }
+    return render(request, "home_page.html", context)
