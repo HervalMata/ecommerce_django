9c641216acd38f7202c8582a070ee0e790450861
HervalMata <hervalmataamparo@gmail.com>
2020-05-12 17:42:24 -0300

    List View

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
