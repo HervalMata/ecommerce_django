c7d6e4525b03da2d46a804e201b91191830a720c
HervalMata <hervalmataamparo@gmail.com>
2020-05-12 23:15:15 -0300

    Products URLs

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -17,9 +17,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 
-from products.views import ProductListView, product_list_view, ProductDetailView, ProductDetailSlugView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView
+#from products.views import ProductListView, product_list_view, ProductDetailView, ProductDetailSlugView, product_detail_view, ProductFeaturedListView, ProductFeaturedDetailView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -27,13 +27,14 @@ urlpatterns = [
     path('', home_page),
     path('login', login_page),
     path('register', register_page),
-    path('products', ProductListView.as_view()),
-    path('products-fbv', product_list_view),
-    url(r'^featured/$', ProductFeaturedListView.as_view()),
-    url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
-    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
-    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
-    url(r'^products/(?P<slug>[\w-]+)/$',  ProductDetailSlugView.as_view()),
+    #path('products', ProductListView.as_view()),
+    #path('products-fbv', product_list_view),
+    #url(r'^featured/$', ProductFeaturedListView.as_view()),
+    #url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
+    #url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    #url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
+    #url(r'^products/(?P<slug>[\w-]+)/$',  ProductDetailSlugView.as_view()),
+    url(r'^products/', include("products.urls")),
     path('about', about_page),
     path('contact', contact_page)
 ]
