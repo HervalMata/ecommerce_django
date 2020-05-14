f7eaf947eee8d3e640cacc5c030c56b47497b35e
HervalMata <hervalmataamparo@gmail.com>
2020-05-14 00:02:33 -0300

    Better Lookups with Q

--- src/products/models.py
+++ src/products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 from .utils import unique_slug_generator
+from django.db.models import Q
 
 
 # Create your models here.
@@ -29,6 +30,12 @@ class ProductQuerySet(models.query.QuerySet):
     def featured(self):
         return self.filter(featured=True, active=True)
 
+    def search(self, query):
+        lookups = (Q(title__icontains=query) |
+                   Q(description__icontains=query) |
+                   Q(price__icontains=query))
+        return self.filter(lookups).distinct()
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
@@ -44,6 +51,9 @@ class ProductManager(models.Manager):
         if qs.count() == 1:
             return qs.first()
         return None
+
+    def search(self, query):
+        return self.get_queryset().active().search(query)
 
 class Product(models.Model):
     title       = models.CharField(max_length=120)
