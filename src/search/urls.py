b15a29d564db3a255e928f05266ce9370732e7ff
HervalMata <hervalmataamparo@gmail.com>
2020-05-13 23:12:08 -0300

    Display the Query to the User

--- src/search/urls.py
+++ src/search/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .views import (SearchProductView)
+app_name='search'
 
 urlpatterns = [
     url(r'^$', SearchProductView.as_view(), name='list')
--- src/search/views.py
+++ src/search/views.py
@@ -4,7 +4,13 @@ from products.models import Product
 
 # Create your views here.
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
@@ -14,7 +20,8 @@ class SearchProductView(ListView):
         if query is not None:
             return Product.objects.filter(title_icontains=query)
         return Product.objects.featured()
-        ...
+        '''
         __icontains = field contains this
         __iexact = field is exactly this
-        ...
+        '''
+
